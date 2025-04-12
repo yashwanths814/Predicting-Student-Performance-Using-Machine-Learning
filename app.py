86499bd5ec8fbfcd093b6bafed6c2d2c9e40d95c
Yashwanth S <yashwanths814@gmail.com>
2025-04-12 14:17:26 +0530

    fix: update prediction route to render 'index.html' instead of 'home.html'

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         print("after prediction")
 
-        return render_template('home.html',results=results[0])
+        return render_template('index.html',results=results[0])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
